538a9da50cf7cc893750abca1ec7c602ad6b1756
ellaj19 <ellaj19@nycstudents.net>
2025-09-25 10:12:18 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,27 @@
-count = 10
+""" count = 10
 
 while count != 0:
     print("This is loop number", count)
-    count = count - 1
+    count = count - 1 """
    
-for i in range(1,10):
-    print(i)
+""" for i in range(1,10):
+    print(i) """
+
+""" x = input("what's your favorite color? type stop to finish.")
+
+while x != ["red","orange","yellow","green","blue","purple","stop"]:  
+    item = input("what's your favorite color?")
+    if x == "stop":
+        print("okay") """
+
+""" color = "" 
+while color != "stop":
+    color = "what's your favorite color? type stop to finish"
+print("thank you for your compliance") """
+
+order = ""
+
+while order != "done":
+    order = input("What would you like to order? (type 'done' to finish): ")
+
+print("Thanks for your order!")
