46559969ab30521935ef9bcaef509d8c0ce1b0cf
ellaj19 <ellaj19@nycstudents.net>
2025-09-26 10:16:12 -0400

    Update test.py

--- test.py
+++ test.py
@@ -13,15 +13,31 @@ while x != ["red","orange","yellow","green","blue","purple","stop"]:
     item = input("what's your favorite color?")
     if x == "stop":
         print("okay") """
-
-""" color = "" 
-while color != "stop":
-    color = "what's your favorite color? type stop to finish"
-print("thank you for your compliance") """
-
+""" 
+ 
 order = ""
 
 while order != "done":
     order = input("What would you like to order? (type 'done' to finish): ")
 
-print("Thanks for your order!")
+print("Thanks for your order!")"""
+
+""" color = ""
+
+while color != "stop":
+    color = input("what's your favorite color? (type stop to finish): ")
+
+print("thank you for your compliance")  """
+
+import random
+RandomNumber = random.randint(1,10)
+guess = False
+answer = int(input("guess a number from 1 to 10"))
+
+while guess == False:
+    
+    if answer == RandomNumber:
+        guess = True
+        print("congrats youre correct")
+    else:
+        answer = int(input("wrong; try again"))
